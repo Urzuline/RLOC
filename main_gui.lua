d0a7eb5ee9ffe3ddd5876ab7a80ad820f13d8c89
Urzuline <106335580+Urzuline@users.noreply.github.com>
2023-04-25 18:49:56 -0400

    Update main_gui.lua

--- main_gui.lua
+++ main_gui.lua
@@ -9,7 +9,7 @@ local pie_booga = {11729688377}
 local other_game = {}
 
 if table.find(pie_booga,game_id) then
-    loadstring("")()  
+    loadstring("https://raw.githubusercontent.com/Urzuline/RLOC/main/pie_booga.lua")()  
 elseif table.find(other_game,game_id) then
     loadstring("")()
 end
