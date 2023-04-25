12c769ae4faebeebf92f4eff97783decf4aabd8e
Urzuline <106335580+Urzuline@users.noreply.github.com>
2023-04-25 18:46:14 -0400

    Update main_gui.lua

--- main_gui.lua
+++ main_gui.lua
@@ -1,1 +1,15 @@
+local game_id = game.PlaceId
 
+--Whitelist stuff (obfuscate this)
+
+
+--
+
+local pie_booga = {11729688377}
+local other_game = {}
+
+if table.find(pie_booga,game_id) then
+    loadstring("")()  
+elseif table.find(other_game,game_id) then
+    loadstring("")()
+end
